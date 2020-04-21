a89e54482a528780c831604e3da36501caf9732c
Easyleases Admin <admin@easyleases.in>
2020-04-21 15:50:31 +0530

    Final Push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBall(),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int randomNo = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Ask me Anything'),
+          backgroundColor: Colors.indigo.shade800,
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.lightBlue,
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  randomNo = Random().nextInt(5) + 1;
+                  print('random $randomNo');
+                });
+              },
+              child: Image(
+                image: AssetImage('images/ball$randomNo.png'),
+                width: 250,
+                height: 250,
+              ),
+            ),
+          ),
+        ));
+  }
+}
